feffc957a4b5c45f6ee95285d3e60e51796d0669
CCxbambix <luisalbertosalazar@ciencias.unam.mx>
2025-05-06 17:24:15 -0600

    Se hicieron cambios en la clase Activo

--- Activo.java
+++ Activo.java
@@ -6,10 +6,18 @@ public class Activo implements EstadoJugador{
     /**Atributo que representa a el jugador  */
     private Jugador jugador;
 
+    /**
+     * Constructor de la clase
+     * @param Jugador 
+     */
     public Activo (Jugador jugador){
         this.jugador = jugador;
     }
 
-    public void borrarPregunta(){
+    /**
+     * Borra la Tarjeta que se mostro del mazo correspondiente
+     */
+    public void borrarPregunta(Tarjeta tarjeta){
+        jugador.mazoActivo.removerCarta(Carta carta);
     }
 }
