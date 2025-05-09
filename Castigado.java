1e06eb8eba84d8b1944a3421ea48b445c2c2ed42
CCxbambix <luisalbertosalazar@ciencias.unam.mx>
2025-05-09 01:38:06 -0600

    Se termino la clase Regular y Castigado

--- Castigado.java
+++ Castigado.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 /**
  * Clase que representa a el estado Castigado del jugador
  */
@@ -8,15 +9,27 @@ public class Castigado implements EstadoJugador{
      */
     private Jugador jugador;
 
+    /**Instancia de la clase Random */
+    private Random random = new Random();
+
+    /**
+     * Metodo constructor de la clase
+     * @param Jugador para crear es estado Castigado
+     */
     public Castigado (Jugador jugador){
         this.jugador = jugador;
     }
 
-     /**
-     * Metodo que rgresara la siguiente pregunta del mazo de cartas
+    /**
+     * Metodo que obtiene la siguiente pregunta para el jugador de la baraja Castigado
+     * Se obtiene la lista de Tarjeta para obtener un elemento de la lista, se usa un metodo que devuelve un numero
+     * random entre 0 y la longitud de la lista, se usa el +1 porque el metodo no incluye el ultimo numero,
+     * asi se asegura que pueda salir tambien la ultima Tarjeta
+     * @return Tarjeta de la baraja Castigado 
      */
     public Tarjeta obtenerSiguientePregunta(){
-        return null;
+        Tarjeta siguienteTarjeta = jugador.getBarajaCastigado().get(random.nextInt(jugador.getBarajaRegular().size()+1));
+        return siguienteTarjeta;
     }
 
     /**
@@ -25,4 +38,6 @@ public class Castigado implements EstadoJugador{
     public void borrarPregunta(Tarjeta tarjeta){
         jugador.getBarajaCastigado().remove(tarjeta);
     }
+
+    
 }
--- Regular.java
+++ Regular.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 /**
  * Clase que representa a el estado regular de un usuario
  */
@@ -5,6 +6,9 @@ public class Regular implements EstadoJugador{
 
     /**Atributo que representa a el jugador  */
     private Jugador jugador;
+
+    /**Instancia de la clase Random */
+    private Random random = new Random();
 
     /**
      * Constructor de la clase
@@ -15,10 +19,15 @@ public class Regular implements EstadoJugador{
     }
 
     /**
-     * Metodo que rgresara la siguiente pregunta del mazo de cartas
+     * Metodo que obtiene la siguiente pregunta para el jugador de la baraja Regular
+     * Se obtiene la lista de Tarjeta para obtener un elemento de la lista, se usa un metodo que devuelve un numero
+     * random entre 0 y la longitud de la lista, se usa el +1 porque el metodo no incluye el ultimo numero,
+     * asi se asegura que pueda salir tambien la ultima Tarjeta
+     * @return Tarjeta de la baraja Regular
      */
     public Tarjeta obtenerSiguientePregunta(){
-        return; null;//falta este metodo
+        Tarjeta siguienteTarjeta = jugador.getBarajaRegular().get(random.nextInt(jugador.getBarajaRegular().size()+1));
+        return siguienteTarjeta;
     }
 
     /**
